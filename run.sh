706f86122413de2845aa85c2a1f0c2ce8f8f6288
Henrik Thorburn <boaworm@gmail.com>
2026-02-22 08:04:00 -0600

    Generalizing a bit. Making sure the story itself is not i the runner code

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #source story_env/bin/activate
 python --version
-python story_writer.py --story story_background.txt --instructions instructions.txt --save_summary summary.txt --new_chapter new_chapter.txt --key_event_chunk_size 4
+python story_writer.py --story story_background.txt --model mistral-small-3.2 --instructions instructions.txt --save_summary summary.txt --new_chapter new_chapter.txt --key_event_chunk_size 4
